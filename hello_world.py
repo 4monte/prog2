44e47b91695947b04b7a77e83ec6c0d0ecf9d07b
4monte <91333608+4monte@users.noreply.github.com>
2021-11-07 15:18:35 +0100

    Test

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,22 @@
-print("Hello World")
+from flask import Flask
+from flask import render_template
+
+app = Flask("Hello World")
+
+
+@app.route("/hello")
+def hello():
+    return render_template('index.html', name="Sofia")
+
+@app.route("/hello2/" , methods=['GET', 'POST'])
+def hallo():
+    if request.method == 'POST':
+        return render_template("index.html")
+    if request.method == 'POST':
+        ziel_person = request.form['vorname']
+        rueckgabe_string = "Hello " + ziel_person + "!"
+        return rueckgabe_string
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
