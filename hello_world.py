69d8a5d9ed1c51e893ef0ee5b3643db2e22c06b1
4monte <91333608+4monte@users.noreply.github.com>
2021-11-11 10:13:05 +0100

    HTML-Struktur begonnen

--- hello_world.py
+++ hello_world.py
@@ -1,22 +1,11 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask("Hello World")
+app = Flask(__name__)
 
-
-@app.route("/hello")
-def hello():
-    return render_template('index.html', name="Sofia")
-
-@app.route("/hello2/" , methods=['GET', 'POST'])
-def hallo():
-    if request.method == 'POST':
-        return render_template("index.html")
-    if request.method == 'POST':
-        ziel_person = request.form['vorname']
-        rueckgabe_string = "Hello " + ziel_person + "!"
-        return rueckgabe_string
-
+@app.route("/" , methods=['GET', 'POST'])
+def Index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
