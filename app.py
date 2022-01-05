6fd4ee5f4d593f88678aa96e9493a19f4d7127c5
4monte <91333608+4monte@users.noreply.github.com>
2022-01-05 18:04:27 +0100

    Erste Implementierung von yfinance um Aktienkurs anzeigen, Code noch nicht funktionsfähig

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+import yfinance as yf
+
+
 
 app = Flask(__name__)
 
 @app.route("/" , methods=['GET', 'POST'])
 def Index():
-    return render_template("index.html")
+
+    if request.method == 'POST':
+        # 3 Variablen an Input Formular zuweisen
+        aktiensymbol = request.form["aktiensymbol"]
+        anzahl = request.form["anzahl"]
+        preis = request.form["preis"]
+        usereingabe = yf.Ticker(aktiensymbol)
+        marktpreis = usereingabe.info["regularMarketPrice"]
+        ausgabe = {
+            "aktiensymbol" : aktiensymbol,
+            "preis" : preis
+            "marktpreis" : marktpreis,
+            "anzahl" : anzahl,
+            "gewinn" : anzahl * float(marktpreis-preis)
+        }
+
+        return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
